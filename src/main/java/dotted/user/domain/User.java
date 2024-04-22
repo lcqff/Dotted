8cf60e343b807e16411f02d5537b1f3a68d0ded6
lcqff <yeonnu82g@gmail.com>
2024-04-22 16:30:11 +0900

    feat: 미작성된 엔티티 작성

--- src/main/java/dotted/user/domain/User.java
+++ src/main/java/dotted/user/domain/User.java
@@ -1,4 +1,51 @@
 package dotted.user.domain;
 
-public class User {
+import dotted.base.BaseEntity;
+import dotted.login.utils.SocialType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long socialId;
+
+    @Column(nullable = false)
+    private SocialType socialType;
+
+    private String email;
+
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserGroup> userGroups;
+
+    @Builder
+    private User(String name, Long socialId, SocialType socialType, String email, String imageUrl,
+                List<UserGroup> userGroups) {
+        this.name = name;
+        this.socialId = socialId;
+        this.socialType = socialType;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.userGroups = userGroups;
+    }
 }
