10c461cc184308ea08f80ba6ce5592e9f2d89b94
lcqff <yeonnu82g@gmail.com>
2024-05-08 14:18:33 +0900

    feat: Spring security 기본 로그인 화면 제거

--- src/main/java/dotted/DottedApplication.java
+++ src/main/java/dotted/DottedApplication.java
@@ -2,8 +2,9 @@ package dotted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DottedApplication {
 
 	public static void main(String[] args) {
